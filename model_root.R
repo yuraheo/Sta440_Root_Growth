172d05576814fd18b637f6ce7e82996a12ae071f
yuraheo <yh346@duke.edu>
2025-11-04 10:37:53 -0500

    model fix

--- model_root.R
+++ model_root.R
@@ -14,9 +14,7 @@ model {
       gamma3 * side[i] * geno[i]
   }
   
-  #------------------------------------------------------
   # Random effects by root (level-2 variation)
-  #------------------------------------------------------
   for (j in 1:n.root) {
     A[j]     ~ dnorm(mu_A, prec_A)
     B[j]     ~ dnorm(mu_B, prec_B)
@@ -24,9 +22,7 @@ model {
     alpha[j] ~ dnorm(mu_alpha, between.prec)
   }
   
-  #------------------------------------------------------
   # Hyperparameters informed by NLS
-  #------------------------------------------------------
   mu_A     ~ dnorm(muA_hat,      pow(prior_sd_A,     -2))
   mu_B     ~ dnorm(muB_hat,      pow(prior_sd_B,     -2))
   mu_alpha ~ dnorm(muAlpha_hat,  pow(prior_sd_alpha, -2))
@@ -35,16 +31,12 @@ model {
   prec_A ~ dgamma(0.001, 0.001)
   prec_B ~ dgamma(0.001, 0.001)
 
-  #------------------------------------------------------
   # Fixed effects (global parameters)
-  #------------------------------------------------------
   gamma1 ~ dnorm(0, 1.0E-2)   # side effect (Outer vs Inner)
   gamma2 ~ dnorm(0, 1.0E-2)   # genotype effect (WT vs MU)
   gamma3 ~ dnorm(0, 1.0E-2)   # interaction effect
   
-  #------------------------------------------------------
   # Variance components and priors
-  #------------------------------------------------------
   within.prec  <- pow(sigma2, -1)
   between.prec <- pow(tau2, -1)
   
@@ -53,3 +45,6 @@ model {
   sigma ~ dunif(0, 10)
   tau   ~ dunif(0, 5)
 }
+
+
+
