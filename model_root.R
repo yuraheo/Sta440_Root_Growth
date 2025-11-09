52e9639e0042cc5e06eaf340c9201890d52e6153
oliviafu <qf35@duke.edu>
2025-11-09 14:00:27 -0500

    new model

--- model_root.R
+++ model_root.R
@@ -8,10 +8,10 @@ model {
     
     # Effective midpoint includes side and genotype effects
     # Outer: side = 1; Mutant: geno = 1
+    # Assume wildtype inner = mutant inner
     xmid_eff[i] <- alpha[root[i]] +
-      gamma1 * side[i] +
-      gamma2 * geno[i] +
-      gamma3 * side[i] * geno[i]
+      gamma1 * side[i] +                # WT outer vs inner
+      gamma2 * side[i] * geno[i]        # extra outer shift for mutant
   }
   
   # Random effects by root
@@ -39,15 +39,15 @@ model {
   # mean of log(B_j - A_j) anchored at log(span)
   mu_d  ~ dnorm(log(L_span), 1)
   
-  # midpoint mean around 0.5 on logit scale (logit(0.5) = 0)
-  # precision = 4 -> sd = 0.5  (midpoints mostly within ~0.2–0.8)
-  mu_z0 ~ dnorm(0, 4)
+  # Midpoint mean around 0.5 on the logit scale (logit(0.5) = 0)
+  # Precision = 1  -> sd = 1  (midpoints mostly within ~0.1–0.9)
+  mu_z0 ~ dnorm(0, 1)
   
   # mean of log(scal) on normalized x axis
   # center at ~0.2 with sd = 0.707 (scale mostly within 0.05-0.8)
   mu_eta_s ~ dnorm(log(0.2), 2)
   
-  # between-root variation (still fairly vague)
+  # between-root variation (fairly vague)
   prec_a      ~ dgamma(1, 1)
   prec_d      ~ dgamma(1, 1)
   prec_z0     ~ dgamma(1, 1)
@@ -60,11 +60,13 @@ model {
   # so we expect shifts of order ≤ half a root length
   gamma1 ~ dnorm(0, 4)
   gamma2 ~ dnorm(0, 4)
-  gamma3 ~ dnorm(0, 4)
 
   #------------------------------------------------------
   # Residual and hierarchical variances
   #------------------------------------------------------
-  within.prec  <- pow(sigma, -2)
-  sigma ~ dunif(0, 10)
+  within.prec <- pow(sigma, -2)
+  
+  # Half-t(df = 3, scale = 3) prior for sigma
+  sigma <- abs(t_sigma)
+  t_sigma ~ dt(0, pow(3, -2), 3)   # mean 0, scale = 3, df = 3
 }
